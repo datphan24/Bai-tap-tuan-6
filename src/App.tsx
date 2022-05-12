3b016081bbe5d5455f80e88a9aa0392001c109c1
DatPhan <tt.dat.phan@tomosia.com>
2022-05-12 14:29:42 +0700

    interface login signup dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route} from 'react-router-dom'
 import './App.css';
+import SignInSide from './components/SignInSide'
+import SignUp from './components/SignUp'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/login' element={<SignInSide />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/interface/interface.ts
@@ -0,0 +1,3 @@
+export interface onclick {
+  preventDefault: Function
+}
